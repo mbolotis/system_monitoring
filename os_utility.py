32e9697e9d695847641ec814ad31c290b0001b06
Bolotis Michail <m.bolotis@exodussa.gr>
2020-04-17 00:21:50 +0300

    Add Critical Path

--- os_utility.py
+++ os_utility.py
@@ -1,4 +1,5 @@
 import time
+from multiprocessing import Process
 import threading
 import psutil
 import os
@@ -10,27 +11,28 @@ except:
 
 def cpu_usage():
     x = psutil.cpu_percent(interval=1, percpu=True)
-    print(type(x))
+    #print(type(x))
 
     return x[0]
 
+
 def ram_usage():
     r_usage = psutil.virtual_memory()[2]
-    print('memory % used:', psutil.virtual_memory()[2])
+    #print('memory % used:', psutil.virtual_memory()[2])
 
     return r_usage
 
 
 def disk_space():
     d_usage = psutil.disk_usage('/')[3]
-    print("Free Disk Space % : ", psutil.disk_usage('/')[3])
+    #print("Free Disk Space % : ", psutil.disk_usage('/')[3])
 
     return d_usage
 
 
-def service_status(service_name):
+def service_status(my_service_name):
     try:
-        y = psutil.win_service_get(service_name)
+        y = psutil.win_service_get(my_service_name)
         print("Binary path : ", y.binpath())
         print("Service Status : ", y.status())
         r_value = y.status()
@@ -41,28 +43,80 @@ def service_status(service_name):
     return r_value
 
 
+def critical_path(*args):
+    temp_value = recalculate_value(args[1])
+    if args[1] == 0:
+        while True:
+            if temp_value < 80:
+                break
+            elif time.time() - args[2] > secs_threshold:
+                return 1  # critical incident
+
+            time.sleep(1)
+            temp_value = recalculate_value(args[0])
+
+        return 0  # clear
+    else:
+        while temp_value > cpu_threshold:
+            time.sleep(1)
+            temp_value = recalculate_value(args[0])
+
+        return 0  # clear
+
+
+def recalculate_value(my_type):
+    if my_type == types[0]:
+        temp_value = cpu_usage()
+    elif my_type == types[1]:
+        temp_value = ram_usage()
+    else:
+        temp_value = disk_space()
+
+    return temp_value
+
+
 if __name__ == '__main__':
+    types = ("cpu", "ram", "disk")
     service_name = 'Dnscache'
     cpu = cpu_usage()
     ram = ram_usage()
     disk = disk_space()
     service = service_status(service_name)
 
-    cpu_threshold = 80
-    ram_threshold = 50
-    disk_threshold = 80
-    service_prop_status = "running"
+    secs_threshold = 20
 
-    conditions = ("Clear", "Incident")
+    cpu_threshold = 80.0
+    ram_threshold = 50.0
+    disk_threshold = 80.0
+    service_prop_status = "running"
 
     t1 = threading.Thread()
 
     while True:
         if cpu > cpu_threshold:
-            print("Poli cpu")
+            now_time = time.time()
+            call = critical_path(types[0], 0, now_time)  # 0 = Incident, 1 = consolidation
+            while call == 1:
+                call = critical_path(types[0], 1)
+            print("Poli cpu", time.time())
         if ram > ram_threshold:
-            print("Poli ram")
+            now_time = time.time()
+            call = critical_path(types[1], 0, now_time)  # 0 = Incident, 1 = consolidation
+            while call == 1:
+                call = critical_path(types[1], 1)
+            print("Poli ram", time.time())
         if disk > disk_threshold:
-            print("Poli disk")
+            now_time = time.time()
+            call = critical_path(types[2], 0, now_time)  # 0 = Incident, 1 = consolidation
+            while call == 1:
+                call = critical_path(types[2], 1)
+            print("Poli disk", time.time())
+
         if service != service_prop_status:
             print("Service down")
+
+        cpu = recalculate_value(types[0])
+        ram = recalculate_value(types[1])
+        disk = recalculate_value(types[2])
+
+        time.sleep(2)
