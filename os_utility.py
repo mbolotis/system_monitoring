19df6189890a13a41e29ef5e9e740d12a77d325a
Michael Bolotis <michael-94@windowslive.com>
2020-04-21 07:14:35 +0300

    Adding thread functionality and email utility

--- os_utility.py
+++ os_utility.py
@@ -1,12 +1,7 @@
+import smtplib, ssl
 import time
-from multiprocessing import Process
 import threading
 import psutil
-import os
-try:
-    import httplib
-except:
-    import http.client as httplib
 
 
 def cpu_usage():
@@ -24,7 +19,7 @@ def ram_usage():
 
 
 def disk_space():
-    d_usage = psutil.disk_usage('/')[3]
+    d_usage = psutil.disk_usage('/')[3]  # free space
     #print("Free Disk Space % : ", psutil.disk_usage('/')[3])
 
     return d_usage
@@ -33,11 +28,11 @@ def disk_space():
 def service_status(my_service_name):
     try:
         y = psutil.win_service_get(my_service_name)
-        print("Binary path : ", y.binpath())
-        print("Service Status : ", y.status())
+        #print("Binary path : ", y.binpath())
+        print("Service Status : {} \n".format(y.status()))
         r_value = y.status()
-    except:
-        print("No such service")
+    except psutil.NoSuchProcess:
+        print("No such service \n")
         r_value = "No such service"
 
     return r_value
@@ -55,7 +50,7 @@ def recalculate_value(my_type):
 
 
 def critical_path(*args):
-    temp_value = recalculate_value(types[1])
+    temp_value = recalculate_value(args[0])
     if args[1] == 0:  # Incident mode
         while True:
             if temp_value < args[3]:
@@ -85,17 +80,19 @@ def ram_calculation():
         # Consolidation stage
         if call == 1:
             consolidation = True
-            print("SEND RAM INCIDENT EMAIL")
+            print("SEND RAM INCIDENT EMAIL \n")
+            email_execution(ram_template_incident)
             '''SEND INCIDENT EMAIL'''
         while call == 1:
             call = critical_path(types[1], 1, None, ram_threshold)  # recalculates values until becoming clear
 
         if consolidation:
-            print("SEND RAM CLEAR EMAIL")
+            print("SEND RAM CLEAR EMAIL \n")
+            email_execution(ram_template_clear)
             '''SEND CLEAR EMAIL'''
         # End of consolidation stage
 
-    print("Ram usage !", ram_value, time.time())
+    print("Ram usage !{} {} \n".format(ram_value, time.time()))
     time.sleep(1)
     ram_calculation()
 
@@ -110,13 +107,15 @@ def cpu_calculation():
         # Consolidation stage
         if call == 1:
             consolidation = True
-            print("SEND CPU INCIDENT EMAIL")
+            print("SEND CPU INCIDENT EMAIL \n")
+            email_execution(cpu_template_incident)
             '''SEND INCIDENT EMAIL'''
         while call == 1:
             call = critical_path(types[0], 1, None, cpu_threshold)  # recalculates values until becoming clear
 
         if consolidation:
-            print("SEND CPU CLEAR EMAIL")
+            print("SEND CPU CLEAR EMAIL \n")
+            email_execution(cpu_template_clear)
             '''SEND CLEAR EMAIL'''
         # End of consolidation stage
 
@@ -125,74 +124,154 @@ def cpu_calculation():
     cpu_calculation()
 
 
+def disk_calculation():
+    consolidation = False
+    disk_value = recalculate_value(types[2])
+    if disk_value > disk_threshold:
+        now_time = time.time()
+        call = critical_path(types[2], 0, now_time, disk_threshold)  # 0 = Incident, 1 = consolidation
+
+        # Consolidation stage
+        if call == 1:
+            consolidation = True
+            print("SEND disk INCIDENT EMAIL \n")
+            email_execution(disk_template_incident)
+            '''SEND INCIDENT EMAIL'''
+        while call == 1:
+            call = critical_path(types[2], 1, None, disk_threshold)  # recalculates values until becoming clear
+
+        if consolidation:
+            print("SEND disk CLEAR EMAIL \n")
+            email_execution(disk_template_clear)
+            '''SEND CLEAR EMAIL'''
+        # End of consolidation stage
+
+    print("Disk usage !", disk_value, time.time())
+    time.sleep(1)
+    disk_calculation()
+
+
+def service_check(my_service_name):
+    consolidation = False
+    temp_value = service_status(my_service_name)
+
+    if temp_value != service_prop_status:
+        start_time = time.time()
+        while temp_value != service_prop_status:
+            temp_value = service_status(my_service_name)
+            if time.time() - start_time < secs_threshold:
+                consolidation = True
+                print("SEND SERVICE INCIDENT EMAIL \n")
+                email_execution(service_template_incident)
+                break
+
+    # consolidation stage
+    if temp_value != service_prop_status and consolidation:
+        while temp_value != service_prop_status:
+            time.sleep(1)
+            temp_value = service_status(my_service_name)
+
+        print("SEND SERVICE CLEAR EMAIL \n")
+        email_execution(service_template_clear)
+
+    time.sleep(1)
+    service_check(service_name)
+
+
+def email_execution(template):
+    try:
+        print("Starting to send email", time.ctime(time.time()))
+        server_ssl.login(sender, passw)
+        server_ssl.sendmail(sender, receiver, template)
+        print("Email sent", time.ctime(time.time()))
+    except smtplib.SMTPDataError:
+        time.sleep(1)
+        email_execution(template)
+
+
 if __name__ == '__main__':
+    #pip install psutil
+    confirmed = "N"
+    while confirmed != "Y" and confirmed != "y":
+        try:
+            server_name = str(input("Please define a name for this system : "))
+            cpu_threshold = float(input("Give me CPU threshold percentage: "))
+            ram_threshold = float(input("Give me RAM threshold percentage: "))
+            disk_threshold = float(input("Give me Disk threshold percentage: "))
+            secs_threshold = int(input("After how many seconds of exceeding, would you like to be notified? "))
 
-    service_name = 'Dnscache'
-    service = service_status(service_name)
-    service_prop_status = "running"
+            if cpu_threshold < 100 and ram_threshold < 100 and disk_threshold < 100:
+                confirmed = input("Do you want to confirm the above configuration ? [Y/N] : ")
+            else:
+                print("Percentages should be less than 100!")
+                confirmed = "N"
+        except ValueError:
+            print("Give me numbers!")
 
-    types = ("cpu", "ram", "disk")
+    types = ("CPU", "RAM", "DISK")
 
-    secs_threshold = 10
-    cpu_threshold = 80.0
-    ram_threshold = 45.0
-    disk_threshold = 80.0
+    add_service = str(input('Do you want to monitor a service ? [Y/N] : '))
+    service_name = None
+
+    while add_service == "Y" or add_service == "y":
+        service_name_temp = str(input("Give me a service to monitor : "))  # MongoDB
+        try:
+            psutil.win_service_get(service_name_temp)
+            service_name = service_name_temp
+            service_prop_status = "running"
+            add_service = "N"
+        except psutil.NoSuchProcess:
+            print("No such service \n")
+            add_service = str(input("Do you want to monitor a service ? [Y/N] : "))
+
+    # Email configuration starts
+
+    login_completed = False
+    while not login_completed:
+        sender = input("Give me the email account : ")
+        passw = input("Password : ")
+        try:
+            port = 465
+            server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", port)
+            server_ssl.login(sender, passw)
+            print("you have logged in!")
+            login_completed = True
+        except:
+            login_failure_reasons = """
+            You did not manage to login to this email for 1 (or more) of the following reasons:\n
+            - You gave invalid credentials
+            - You do not have enabled the following value  https://myaccount.google.com/lesssecureapps?pli=1
+            - You are using two-factor authentication to login to this email account 
+            """
+            print(login_failure_reasons)
+
+    # receiver = str(input("Give me the email address that will be notified : "))
+    receiver = sender
+
+    # Define the email templates
+    cpu_template_incident = 'Subject: Monitor Notification: Server {} {} Usage INCIDENT\n\nServer {}, {} using more than {}% of its resources for more than {} seconds!'.format(server_name, types[0], server_name, types[0], int(cpu_threshold), secs_threshold)
+    ram_template_incident = 'Subject: Monitor Notification: Server {} {} Usage INCIDENT\n\nServer {}, {} using more than {}% of its resources for more than {} seconds!'.format(server_name, types[1], server_name, types[1], int(ram_threshold), secs_threshold)
+    disk_template_incident = 'Subject: Monitor Notification: Server {} {} Usage INCIDENT\n\nServer {}, {} using more than {}% of its resources for more than {} seconds!'.format(server_name, types[2], server_name, types[2], int(disk_threshold), secs_threshold)
+    service_template_incident = 'Subject: Monitor Notification: Server {} Service {} has stopped\n\nServer {} Service {} has stopped!'.format(server_name, service_name, server_name, service_name)
+
+    cpu_template_clear = 'Subject: Monitor Notification: Server {} {} Usage CLEAR\n\nServer {}, {} is not using more than {}% of its resources, anymore!'.format(server_name, types[0], server_name, types[0], int(cpu_threshold))
+    ram_template_clear = 'Subject: Monitor Notification: Server {} {} Usage CLEAR\n\nServer {}, {} is not using more than {}% of its resources, anymore!'.format(server_name, types[1], server_name, types[1], int(ram_threshold))
+    disk_template_clear = 'Subject: Monitor Notification: Server {} {} Usage CLEAR\n\nServer {}, {} is not using more than {}% of its resources, anymore!'.format(server_name, types[2], server_name, types[2], int(disk_threshold))
+    service_template_clear = 'Subject: Monitor Notification: Server {} Service {} is running\n\nServer {} Service {} is running!'.format(server_name, service_name, server_name, service_name)
+    # End email configuration
+
+    print("Program has started to run")
+
+    if service_name is not None:
+        service_thread = threading.Thread(target=service_check, args=(service_name,))
+        service_thread.start()
 
     cpu_thread = threading.Thread(target=cpu_calculation, args=())
     ram_thread = threading.Thread(target=ram_calculation, args=())
+    disk_thread = threading.Thread(target=disk_calculation, args=())
 
     cpu_thread.start()
     ram_thread.start()
+    disk_thread.start()
 
-    while True:
-        time.sleep(10)
-
-
-'''    ram_thread = threading.Thread(target=ram_calculation(), daemon=True)
-    cpu_thread = threading.Thread(target=cpu_calculation(), daemon=True)
-
-    ram_thread.start()
-    cpu_thread.start()'''
-
-'''        if not ram_thread.is_alive():
-            ram_thread.start()
-            #ram_thread.join(timeout=2)
-        if not cpu_thread.is_alive():
-            cpu_thread.start()
-            #cpu_thread.join(timeout=2)
-
-        ram_thread.join(timeout=2)
-        cpu_thread.join(timeout=2)'''
-        #time.sleep(2)
-
-        #Process(target=ram_calculation()).start()
-        #Process(target=cpu_calculation()).start()
-        #ram_calculation()
-'''        cpu = recalculate_value(types[0])
-        #ram = recalculate_value(types[1])
-        disk = recalculate_value(types[2])
-
-        if cpu > cpu_threshold:
-            now_time = time.time()
-            call = critical_path(types[0], 0, now_time)  # 0 = Incident, 1 = consolidation
-            while call == 1:
-                call = critical_path(types[0], 1)
-            print("Poli cpu", time.time())
-        if ram > ram_threshold:
-            now_time = time.time()
-            call = critical_path(types[1], 0, now_time)  # 0 = Incident, 1 = consolidation
-            while call == 1:
-                call = critical_path(types[1], 1)
-            print("Poli ram", time.time())
-        if disk > disk_threshold:
-            now_time = time.time()
-            call = critical_path(types[2], 0, now_time)  # 0 = Incident, 1 = consolidation
-            while call == 1:
-                call = critical_path(types[2], 1)
-            print("Poli disk", time.time())
-
-        if service != service_prop_status:
-            print("Service down")'''
-
-
-# sxolia, threading, emails
+# background, readme
